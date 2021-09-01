d53d70f77805b62c1b4e6964edba32df01f66163
bruce30709 <bruce30709@gmail.com>
2021-09-01 20:40:50 +0800

    change to base64

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def handle_message(event):
             line_bot_api.reply_message(event.reply_token,  message)
         if b[0]=="1":
             a = Base64()
-            message=TextSendMessage(a.Decode(b[1]," ").decode())
+            message=TextSendMessage(a.Decode(b[1]," ").decode('utf-8'))
             print(message)
             line_bot_api.reply_message(event.reply_token,  message)
             #print(ODF_data)
