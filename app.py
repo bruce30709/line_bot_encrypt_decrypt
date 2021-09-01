c3d2ab198b853be0763403a919444ae456ec7dc2
bruce30709 <50272611+bruce30709@users.noreply.github.com>
2021-09-01 22:14:51 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 # -*- coding: UTF-8 -*-
 from flask import Flask, request, abort
-import DAN, csmapi, random, time, threading
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
-
+from mybase64 import *
 app = Flask(__name__)
 
 # Channel Access Token
@@ -39,33 +38,32 @@ def handle_message(event):
         b = a.split(' ')
         if b[0]=="0":
             print("0")
-            DAN.push('str0',b[1])
+            #DAN.push('str0',b[1])
         if b[0]=="1":
             print("1")
-            DAN.push('str1',b[1])
+            #DAN.push('str1',b[1])
          #Push data to an input device feature "Status"
         #==================================
+        '''
         ODF_data= DAN.pull('str2') #Pull data from an output device feature "Name-O"
         if ODF_data :
             message = TextSendMessage(str(ODF_data[0]))
             line_bot_api.reply_message(event.reply_token, message)
         else :
-            if b[0]=='0':
-                k =  'djq%5cu#-jeq15abg$z9_i#_w=$o88m!*alpbedlbat8cr74sd'
-                encry_str = ""
-                for i,j in zip(b[1],k):
-                    temp = str(ord(i)+ord(j))+'_'
-                    encry_str = encry_str + temp
-                message = TextSendMessage(encry_str)
+        '''
+        if b[0]=='0':
+            a = Base64()
+            message=TextSendMessage(a.Encode(str.encode(b[1])))
+            print(message)
+            line_bot_api.reply_message(event.reply_token,  message)
+        if b[0]=="1":
+            a = Base64()
+            message=TextSendMessage(a.Decode(b[1]," ").decode("ISO-8859-1"))
+            
+            if message:
+                print(message)
                 line_bot_api.reply_message(event.reply_token,  message)
-            if b[0]=="1":
-                k = 'djq%5cu#-jeq15abg$z9_i#_w=$o88m!*alpbedlbat8cr74sd'
-                dec_str = ""
-                for i,j in zip(b[1].split("_")[:-1],k):
-                    temp = chr(int(i) - ord(j))
-                    dec_str = dec_str+temp
-                message = TextSendMessage(dec_str)
-                line_bot_api.reply_message(event.reply_token,  message)
+            
             #print(ODF_data)
     # you can write some codes here to handle the message sent by users
     #try:
@@ -78,11 +76,6 @@ def handle_message(event):
             #print(ODF_data)
     except Exception as e:
         print(e)
-        if str(e).find('mac_addrnot found:') != -1:
-            print('Reg_addris not found. Try to re-register...')
-            DAN.device_registration_with_retry(ServerURL, Reg_addr)
-        else:
-            print('Connection failed due to unknown reasons.')
 
 import os
 
@@ -98,13 +91,13 @@ if __name__ == "__main__":
     # Register
 
     # Deregister
-    ServerURL = "http://140.114.77.90:9999"  # with non-secure connection
+    '''ServerURL = "http://140.114.77.90:9999"  # with non-secure connection
     Reg_addr = "7788"  # if None, Reg_addr= MAC address
 
     DAN.profile["dm_name"] = "109062599_DM"
     DAN.profile['df_list']=[ 'str0','str1', 'str2']
     # DAN.profile['d_name']= 'Assign a Device Name'
 
-    DAN.device_registration_with_retry(ServerURL, Reg_addr)
+    DAN.device_registration_with_retry(ServerURL, Reg_addr)'''
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
---
+++ mybase64.py
@@ -0,0 +1,60 @@
+class Base64():
+
+    def __init__(self):
+
+        ## We only need to do this once
+        self.b64 = ["v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+         "K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","!","\"",
+         "#","$","%","&",chr(0x27),"(",")","*","+",",","-",".","/",":",";","<","=",
+         ">","?","@","[","\\","]","^","_","`","{","|","}","~","o"]
+
+    def Encode(self, data):
+        alphabet = self.b64
+        bit_str = ""
+        base64_str = ""
+
+        for char in data:
+            bin_char = bin(char).lstrip("0b")
+            bin_char = bin_char.zfill(8)
+            bit_str += bin_char
+
+        brackets = [bit_str[x:x+6] for x in range(0,len(bit_str),6)]
+
+        for bracket in brackets:
+            if(len(bracket) < 6):
+                bracket = bracket + (6-len(bracket))*"0"
+            base64_str += alphabet[int(bracket,2)]
+
+        ##Add padding characters to maintain compatibility with forced padding
+        padding_indicator = len(base64_str) % 4
+        if padding_indicator == 3:
+            base64_str += "="
+        elif  padding_indicator == 2:
+            base64_str += "=="
+
+        return base64_str
+
+    def Decode(self, text, eof):
+        alphabet = self.b64
+        bit_str = ""
+        text_str = ""
+
+        for char in text:
+            if char in alphabet:
+                bin_char = bin(alphabet.index(char)).lstrip("0b")
+                bin_char = bin_char.zfill(6)
+                bit_str += bin_char
+
+        brackets = [bit_str[x:x+8] for x in range(0,len(bit_str),8)]
+
+        for bracket in brackets:
+            ## When eof ignore last value in brackets to remove \x00
+            if eof and brackets[len(brackets) -1] == bracket:
+                pass
+            else:
+                text_str += chr(int(bracket,2))
+
+        ## encode string as Latin-1 == ISO-8859-1
+        return text_str.encode("ISO-8859-1")
+
+
